84dc289740c79c7701d9f2e50ddb13c92a676ecf
Yasuhisa HONDA <nqvno14@gmail.com>
2025-02-11 14:56:20 +0900

    add ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <head>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2516886759191822" crossOrigin="anonymous"></script>
+            </head>
         <body className={notojp.className}
             //className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
